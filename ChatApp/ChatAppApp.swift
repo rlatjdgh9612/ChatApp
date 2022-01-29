405d836ce7f39988df18f8fe960f2d32bca568ea
SeongHoKim <rlatjdgh9612@naver.com>
2022-01-29 19:13:28 +0900

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by SeongHoKim on 2022/01/29.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
